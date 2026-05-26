2f9722949457d2a9b342488f51f377fbca8b7683
Lucente Corporate <lucentecorporate@gmail.com>
2026-05-26 15:34:39 +1000

    add firebase config

--- firebase.js
+++ firebase.js
@@ -9,14 +9,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "PASTE_API_KEY_HERE",
-  authDomain: "PASTE_AUTH_DOMAIN_HERE",
-  projectId: "PASTE_PROJECT_ID_HERE",
-  storageBucket: "PASTE_STORAGE_BUCKET_HERE",
-  messagingSenderId: "PASTE_MESSAGING_SENDER_ID_HERE",
-  appId: "PASTE_APP_ID_HERE"
+  apiKey: "AIzaSyAlK07YBdEMF6CD7gCX2PPbqpuxprzl_n8",
+  authDomain: "lucente-work-hub.firebaseapp.com",
+  projectId: "lucente-work-hub",
+  storageBucket: "lucente-work-hub.firebasestorage.app",
+  messagingSenderId: "456942687136",
+  appId: "1:456942687136:web:f6b613cf48212ff5eacac5"
 };
-
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
