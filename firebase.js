b090844b9b8fcfb90ede4cd3301075b38323bba4
Lucente Corporate <lucentecorporate@gmail.com>
2026-05-26 16:32:51 +1000

    fire

--- firebase.js
+++ firebase.js
@@ -1,11 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-// Your web app's Firebase configuration
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAlK07YBdEmF6CD7gCX2PPbqpuxprzl_m8",
+  apiKey: "AIzaSyAlK07YBdEMF6CD7gCX2PPbqpuxprzl_n8",
   authDomain: "lucente-work-hub.firebaseapp.com",
   projectId: "lucente-work-hub",
   storageBucket: "lucente-work-hub.firebasestorage.app",
@@ -13,5 +17,7 @@ const firebaseConfig = {
   appId: "1:456942687136:web:f6b613cf48212ff5eacac5"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
